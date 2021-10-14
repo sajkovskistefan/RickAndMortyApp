a63462346c996c629655e60cf4176bd3b0054ab3
sajkovskistefan <sajkovskistefan@gmail.com>
2021-10-14 18:49:42 +0200

    Edit character page

--- src/Components/Pages/Character/index.js
+++ src/Components/Pages/Character/index.js
@@ -3,6 +3,8 @@ import { useLocation } from 'react-router-dom';
 
 import { getCharacter } from '../../Services/RestAPI';
 
+import './character.css'
+
 const Character = () => {
     const [character, setCharacter] = useState();
     const cat = 'character';
@@ -17,8 +19,22 @@ const Character = () => {
     },[]);
 
     return (
-        <div>
-            
+        <div className="character-page-container">
+            {character ? 
+                <div className="character-container">
+                    <div className="image-div">
+                        <img className="character-image" src={character.image} alt="character image" />
+                    </div>
+                    <div className="character-info">
+                        <span>{character.gender}</span>
+                        <span>{character.location.name}</span>
+                        <span>{character.origin.name}</span>                    
+                        <span>{character.species}</span>                    
+                        <span>{character.status}</span>
+                        <span>{character.type}</span>
+                    </div>
+                </div>
+            :null}
         </div>
     );
 };
