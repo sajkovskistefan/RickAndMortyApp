1893bb6de5d54f62334cb3d962d32cd9ca91161d
sajkovskistefan <sajkovskistefan@gmail.com>
2021-10-15 19:58:11 +0200

    Creating episode component

--- src/Components/Pages/Episodes/index.js
+++ src/Components/Pages/Episodes/index.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getData } from '../../Services/RestAPI';
+
+import './episodes.css'
 
 const Episodes = () => {
+    const [episodes, setEpisodes] = useState();
+    const [number, setNumber] = useState(1)
+    const cat = "episode";
+
+    useEffect(() => {
+        getData(cat,number)
+        .then(data => setEpisodes(data.results))
+    },[]);
+
+    console.log(episodes)
     return (
-        <div>
-            Episodes
+        <div className="page-container">
+            <h1 className="page-header">Episodes</h1>
+            {episodes ? episodes.map((episode) => {
+                return (
+                    <div className="episode-container" key={episode.id}>
+                        <p className="dynaminc-value">{episode.id})</p>
+                        <p className="static-value"> Episode name: </p> <p className="dynaminc-value"> {episode.name} </p>
+                        <p className="static-value"> Air date: </p> <p className="dynaminc-value"> {episode.air_date} </p>
+                        <p className="info-span"> info</p>
+                    </div>
+                )
+            })
+            :null}
         </div>
     );
 };
